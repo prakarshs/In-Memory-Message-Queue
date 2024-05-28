b909ba2e1e8635ed9c744810f078d837501d4993
jhethia <jhethia@gmail.com>
2024-05-28 14:27:20 +0530

    initialized project

---
+++ src/main/java/org/prakarshs/Message.java
@@ -0,0 +1,13 @@
+package org.prakarshs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String message;
+}
+
